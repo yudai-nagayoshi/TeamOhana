1aa0c6c01b336d188d976ae2943222998d2baeca
froide-nagayoshi <yudai-nagayoshi@froide.co.jp>
2021-05-25 14:01:20 +0900

    環境構築追加

---
+++ docker/database/initdb.d/employeelist.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: employeelist_mysql_host
+-- 生成日時: 2021 年 5 月 15 日 18:17
+-- サーバのバージョン： 5.7.34
+-- PHP のバージョン: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `employee_db`
+--
+CREATE DATABASE IF NOT EXISTS `employee_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `employee_db`;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `employee_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL COLLATE utf8mb4_unicode_ci,
+  `furigana` varchar(20) NOT NULL COLLATE utf8mb4_unicode_ci,
+  `email` varchar(40) NOT NULL COLLATE utf8mb4_unicode_ci,
+  `tell` varchar(20) NOT NULL COLLATE utf8mb4_unicode_ci,
+  `joining_date` date NOT NULL,
+  `position_id` int NOT NULL FOREIGN KEY positions(position_id),
+  `department_id` int NOT NULL FOREIGN KEY departments(department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `positions`
+--
+
+DROP TABLE IF EXISTS `positions`;
+CREATE TABLE `positions` (
+  `position_id` int(11) NOT NULL,
+  `position` varchar(20) NOT NULL COLLATE utf8mb4_unicode_ci,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `positions`
+--
+ALTER TABLE `positions`
+  ADD PRIMARY KEY (`position_id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `positions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `department_id` int(11) NOT NULL,
+  `department` varchar(20) NOT NULL COLLATE utf8mb4_unicode_ci,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `departments`
+--
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
