775ec3b747eaff5e0d1680166d4c8aae67456837
froide-nagayoshi <yudai-nagayoshi@froide.co.jp>
2021-05-31 10:01:48 +0900

    add:main.html

--- src/employeeListApp/src/main/java/jp/co/froide/employeeListApp/controller/EmployeeController.java
+++ src/employeeListApp/src/main/java/jp/co/froide/employeeListApp/controller/EmployeeController.java
@@ -1,9 +1,12 @@
-package jp.co.froide.employeeListApp.contoroller;
+package jp.co.froide.employeeListApp.controller;
 
 import jp.co.froide.employeeListApp.dao.EmployeeDao;
 import jp.co.froide.employeeListApp.entity.Employee;
+import jp.co.froide.employeeListApp.entity.EmployeeAll;
+import org.seasar.doma.ResultSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -16,47 +19,62 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
-    @GetMapping("employees")
-    public List<Employee> employeeList() {
+    @GetMapping
+    public List<EmployeeAll> employeeLis() {
         return employeeDao.selectAll();
     }
 
-    @PostMapping("employees")
-    public void insertEmployee(@RequestParam ("name") String name, @RequestParam ("furigana") String furigana,
-                               @RequestParam ("joining_date") String joining_date,
-                               @RequestParam ("position_id") Integer position_id, @RequestParam ("department_id") Integer department_id,
-                               @RequestParam ("pone_number") String phone_number, @RequestParam ("email") String email) {
-        Employee employee = new Employee();
-        employee.setName(name);
-        employee.setFurigana(furigana);
-        employee.setJoining_date(joining_date);
-        employee.setPosition_id(position_id);
-        employee.setDepartment_id(department_id);
-        employee.setPhone_number(phone_number);
-        employee.setEmail(email);
-        employeeDao.insert(employee);
+
+    @GetMapping("detail/{id}")
+    public String test(@PathVariable("id") Integer id,Model model){
+        Employee list = employeeDao.selectById(id);
+        model.addAttribute("list",list);
+        return "detail";
     }
 
-    @PutMapping("employee/{id}")
-    public void updateEmployee(@PathVariable("id") Integer id, @RequestParam ("name") String name, @RequestParam ("furigana") String furigana,
-                               @RequestParam ("joining_date") String joining_date,
-                               @RequestParam ("position_id") Integer position_id, @RequestParam ("department_id") Integer department_id,
-                               @RequestParam ("pone_number") String phone_number, @RequestParam ("email") String email) {
-        Employee employee = employeeDao.selectById(id);
-        employee.setName(name);
-        employee.setFurigana(furigana);
-        employee.setJoining_date(joining_date);
-        employee.setPosition_id(position_id);
-        employee.setDepartment_id(department_id);
-        employee.setPhone_number(phone_number);
-        employee.setEmail(email);
-        employeeDao.update(employee);
-    }
+//    @GetMapping("detail")
+//    public String detail(Model model){
+//        Employee detail = employeeDao.selectById(1);
+//        model.addAttribute("detail",detail);
+//        return "detail";
+//    }
 
-    @DeleteMapping("employee/{id}")
-    public void deleteEmployee(@PathVariable("id") Integer id) {
-        Employee employee = employeeDao.selectById(id);
-        employeeDao.delete(employee);
+//    @PostMapping("employees")
+//    public void insertEmployee(@RequestParam ("name") String name, @RequestParam ("furigana") String furigana,
+//                               @RequestParam ("joining_date") String joining_date,
+//                               @RequestParam ("position_id") Integer position_id, @RequestParam ("department_id") Integer department_id,
+//                               @RequestParam ("pone_number") String phone_number, @RequestParam ("email") String email) {
+//        Employee employee = new Employee();
+//        employee.setName(name);
+//        employee.setFurigana(furigana);
+//        employee.setJoining_date(joining_date);
+//        employee.setPosition_id(position_id);
+//        employee.setDepartment_id(department_id);
+//        employee.setPhone_number(phone_number);
+//        employee.setEmail(email);
+//        employeeDao.insert(employee);
+//    }
+
+//    @PutMapping("employee/{id}")
+//    public void updateEmployee(@PathVariable("id") Integer id, @RequestParam ("name") String name, @RequestParam ("furigana") String furigana,
+//                               @RequestParam ("joining_date") String joining_date,
+//                               @RequestParam ("position_id") Integer position_id, @RequestParam ("department_id") Integer department_id,
+//                               @RequestParam ("pone_number") String phone_number, @RequestParam ("email") String email) {
+//        Employee employee = employeeDao.selectById(id);
+//        employee.setName(name);
+//        employee.setFurigana(furigana);
+//        employee.setJoining_date(joining_date);
+//        employee.setPosition_id(position_id);
+//        employee.setDepartment_id(department_id);
+//        employee.setPhone_number(phone_number);
+//        employee.setEmail(email);
+//        employeeDao.update(employee);
+//    }
+
+    @DeleteMapping("detail/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id, Model model) {
+
+        return "detail";
     }
 }
 
---
+++ src/employeeListApp/src/main/java/jp/co/froide/employeeListApp/dto/EmployeeDto.java
@@ -0,0 +1,2 @@
+package jp.co.froide.employeeListApp.dto;public class EmployeeDto {
+}
